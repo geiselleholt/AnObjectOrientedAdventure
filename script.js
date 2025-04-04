df0ade04337c38e3f0fe6bf630b2eea65014a765
Geiselle Holt <geiselleholt@hotmail.com>
2025-04-03 19:34:20 -0600

    Part 1 and Part 2 done

---
+++ script.js
@@ -0,0 +1,53 @@
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+  companion: {
+    name: "Leo",
+    type: "Cat",
+    companion: {
+      name: "Frank",
+      type: "Flea",
+      inventory: ["small hat", "sunglasses"],
+    },
+  },
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  },
+};
+
+//we could find a sword at adventurer.inventory[0]
+
+// create a loop that logs each item in Robin’s inventory.
+
+// let robinInventory = adventurer.inventory;
+// robinInventory.forEach((item) => {
+//   console.log(item);
+// });
+
+// Test this method by calling adventurer.roll() a few times.
+// adventurer.roll()
+
+// Add the roll method to the Character class.
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+  }
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  }
+}
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+robin.roll()
